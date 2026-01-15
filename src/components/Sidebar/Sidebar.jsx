dbc7b72377cee8baba106b56353fed3d4b055f41
andyshen <chesterccj483@gmail.com>
2026-01-15 19:40:21 +0800

    wip: sidebar function

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,13 +2,25 @@ import styles from './Sidebar.module.css'
 
 function Sidebar(){
     return (
-        <div className={styles.sidebar}>
+        <aside className={styles.sidebar}>
             {/* 這裡可以放大頭貼/文章類別/標籤 */}
-            <div className={styles.menuItem}>Test1</div>
+            <ProfileSection />
             <div className={styles.menuItem}>Test2</div>
             <div className={styles.menuItem}>Test3</div>
-        </div>
+        </aside>
     );
+}
+
+function ProfileSection(){
+    return (
+        <div className={styles.profileSection}>
+            <div className={styles.avatarWrapper}>
+
+            </div>
+            <h3 className={styles.name}>沈柏安</h3>
+            <p className={styles.bio}>資安工程師 | 象棋愛好者</p>
+        </div>
+    )
 }
 
 export default Sidebar;
