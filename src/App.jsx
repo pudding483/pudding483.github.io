cd5c41afb306a5c3180af301759cb603d8df9680
andyshen <chesterccj483@gmail.com>
2026-01-19 16:54:30 +0800

    chore: add react-router-dom and fix indentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header/Header.jsx';
 import BodyWrapper from './layouts/BodyWrapper/BodyWrapper.jsx';
 import Footer from './components/Footer/Footer.jsx';
-
 import './App.css';
 
+function App() {
+    /* Ties all component together */
+    return (
+        <div className="appContainer">
+            <Header />
 
-function App() { /* Ties all component together */
-  return (
-    <div className="appContainer">
-      <Header/>
+            <Routes>
+                <Route path="/" element={<BodyWrapper></BodyWrapper>}>
+                    <Route index element={<Home></Home>} />
+                    <Route path="archive" element={<Archive></Archive>} />
+                    <Route path="about" element={<About></About>} />
+                </Route>
+            </Routes>
 
-      <BodyWrapper />
-      {/* <Sidebar> + <Content> */}
-      
-      <Footer/>
-    </div>
-  );
+            <BodyWrapper />
+            {/* <Sidebar> + <Content> */}
+
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,10 +1,15 @@
+import { Outlet } from 'react-router-dom';
+
 import styles from './Content.module.css';
 
-function Content(){
+function Content() {
     return (
         <div className={styles.container}>
-            <h2 className={styles.title}>主要內容</h2>
-            <p>測試內容...</p>
+            {/* <h2 className={styles.title}>主要內容</h2> */}
+            {/* <p>測試內容...</p> */}
+
+            <Outlet />
+            {/* 會顯示 router match 到的 component */}
         </div>
     );
 }
